732361f2f69a425ce5c9a975a377808700ca2a18
Daniel Kesler <kesler.daniel@gmail.com>
2015-04-23 01:19:21 +0200

    - fix: removed FAKEROOT_ENV

--- package/fabui/fabui.mk
+++ package/fabui/fabui.mk
@@ -27,8 +27,8 @@ FABUI_HTDOCS_FILES = \
 FABUI_DBFILE = fabtotum.db
 
 define FABUI_INSTALL_LIGHTTPD_FILES
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -d -m 0644 $(@D)/recovery/install/system/etc
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -D -m 0644 $(@D)/recovery/install/system/etc/lighttpd/conf-available/99-fabui.conf \
+	$(FABUI_FAKEROOT) $(INSTALL) -d -m 0644 $(@D)/recovery/install/system/etc
+	$(FABUI_FAKEROOT) $(INSTALL) -D -m 0644 $(@D)/recovery/install/system/etc/lighttpd/conf-available/99-fabui.conf \
 		$(FABUI_TARGET_DIR)/etc/lighttpd/conf-available/99-fabui.conf
 endef
 
@@ -38,20 +38,20 @@ endef
 
 define FABUI_INSTALL_TARGET_CMDS
 #	Copy public htdocs files
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -d -o 33 -g 33 -m 0755 $(FABUI_TARGET_DIR)/var/www
+	$(FABUI_FAKEROOT) $(INSTALL) -d -o 33 -g 33 -m 0755 $(FABUI_TARGET_DIR)/var/www
 	for file in $(FABUI_HTDOCS_FILES); do \
-		$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV)  cp -a $(@D)/$$file $(FABUI_TARGET_DIR)/var/www; \
+		$(FABUI_FAKEROOT) cp -a $(@D)/$$file $(FABUI_TARGET_DIR)/var/www; \
 	done
 #	Create runtime data directory
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -d -m 0755 $(FABUI_TARGET_DIR)/var/lib/fabui
+	$(FABUI_FAKEROOT) $(INSTALL) -d -o 33 -g 33 -m 0755 $(FABUI_TARGET_DIR)/var/lib/fabui
 #	The autoinstall flag file is created at compile time
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV)  touch $(FABUI_TARGET_DIR)/var/www/AUTOINSTALL
+	$(FABUI_FAKEROOT) touch $(FABUI_TARGET_DIR)/var/www/AUTOINSTALL
 #	We still need a temp directory for fab_ui_security
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -d -m 0777 $(FABUI_TARGET_DIR)/var/www/temp
+	$(FABUI_FAKEROOT) $(INSTALL) -d -m 0777 $(FABUI_TARGET_DIR)/var/www/temp
 #	Generate Database
 	$(FABUI_INSTALL_DB)
 #	Fix permissions
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) chown 33:33 -R $(FABUI_TARGET_DIR)/var/www
+	$(FABUI_FAKEROOT) chown 33:33 -R $(FABUI_TARGET_DIR)/var/www
 #	Install lighttpd configuration files	
 	$(FABUI_INSTALL_LIGHTTPD_FILES)
 #	Install sudoers permission files
@@ -59,34 +59,34 @@ define FABUI_INSTALL_TARGET_CMDS
 endef
 
 define FABUI_INSTALL_INIT_SYSV
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) mkdir -p $(FABUI_TARGET_DIR)/etc/init.d
+	$(FABUI_FAKEROOT) mkdir -p $(FABUI_TARGET_DIR)/etc/init.d
 		
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -D -m 0775 $(BR2_EXTERNAL)/package/fabui/fabtotum.init \
+	$(FABUI_FAKEROOT) $(INSTALL) -D -m 0775 $(BR2_EXTERNAL)/package/fabui/fabtotum.init \
 		$(FABUI_TARGET_DIR)/etc/init.d/fabtotum
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -D -m 0775 $(BR2_EXTERNAL)/package/fabui/fabui.init \
+	$(FABUI_FAKEROOT) $(INSTALL) -D -m 0775 $(BR2_EXTERNAL)/package/fabui/fabui.init \
 		$(FABUI_TARGET_DIR)/etc/init.d/fabui
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) $(INSTALL) -D -m 0775 $(BR2_EXTERNAL)/package/fabui/fabui.first \
+	$(FABUI_FAKEROOT) $(INSTALL) -D -m 0775 $(BR2_EXTERNAL)/package/fabui/fabui.first \
 		$(FABUI_TARGET_DIR)/etc/firstboot.d/fabui		
 	
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) mkdir -p $(FABUI_TARGET_DIR)/etc/rc.d/rc.firstboot.d
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) ln -fs ../../firstboot.d/fabui \
+	$(FABUI_FAKEROOT) mkdir -p $(FABUI_TARGET_DIR)/etc/rc.d/rc.firstboot.d
+	$(FABUI_FAKEROOT) ln -fs ../../firstboot.d/fabui \
 		$(FABUI_TARGET_DIR)/etc/rc.d/rc.firstboot.d/S10fabui
 	
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) mkdir -p $(FABUI_TARGET_DIR)/etc/rc.d/rc.startup.d	
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) ln -fs ../../init.d/fabtotum \
+	$(FABUI_FAKEROOT) mkdir -p $(FABUI_TARGET_DIR)/etc/rc.d/rc.startup.d	
+	$(FABUI_FAKEROOT) ln -fs ../../init.d/fabtotum \
 		$(FABUI_TARGET_DIR)/etc/rc.d/rc.startup.d/S30fabtotum
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) ln -fs ../../init.d/fabui \
+	$(FABUI_FAKEROOT) ln -fs ../../init.d/fabui \
 		$(FABUI_TARGET_DIR)/etc/rc.d/rc.startup.d/S40fabui
 	
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) mkdir -p $(FABUI_TARGET_DIR)/etc/rc.d/rc.shutdown.d
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) ln -fs ../../init.d/fabui \
+	$(FABUI_FAKEROOT) mkdir -p $(FABUI_TARGET_DIR)/etc/rc.d/rc.shutdown.d
+	$(FABUI_FAKEROOT) ln -fs ../../init.d/fabui \
 		$(FABUI_TARGET_DIR)/etc/rc.d/rc.shutdown.d/S20fabui
-	$(FABUI_FAKEROOT) $(FABUI_FAKEROOT_ENV) ln -fs ../../init.d/fabtotum \
+	$(FABUI_FAKEROOT) ln -fs ../../init.d/fabtotum \
 		$(FABUI_TARGET_DIR)/etc/rc.d/rc.shutdown.d/S98fabtotum
 endef
 
 define FABUI_POST_TARGET_CLEANUP
-	$(FABUI_FAKEROOT) -- rm -rf $(FABUI_TARGET_DIR)/var/www/recovery/install/system/etc
+#	$(FABUI_FAKEROOT) rm -rf $(FABUI_TARGET_DIR)/var/www/recovery/install/system/etc
 endef
 
 FABUI_POST_INSTALL_TARGET_HOOKS += FABUI_POST_TARGET_CLEANUP
@@ -94,7 +94,7 @@ FABUI_POST_INSTALL_TARGET_HOOKS += FABUI_POST_TARGET_CLEANUP
 define FABUI_INSTALL_SUDOERS
 	$(FABUI_FAKEROOT) $(INSTALL) -d -m 0750 $(FABUI_TARGET_DIR)/etc/sudoers.d
 	$(FABUI_FAKEROOT) $(INSTALL) -D -m 0440 $(BR2_EXTERNAL)/package/fabui/fabui.sudoers $(FABUI_TARGET_DIR)/etc/sudoers.d/fabui
-	$(FABUI_FAKEROOT) -- chmod 0440 $(FABUI_TARGET_DIR)/etc/sudoers.d/fabui
+	$(FABUI_FAKEROOT) chmod 0440 $(FABUI_TARGET_DIR)/etc/sudoers.d/fabui
 endef
 
 $(eval $(generic-package))
