3aca4beddf881d4748ec789494af57ec6df7a8d6
Simone Cociancich <coch@localhost.localdomain>
2015-04-23 15:11:03 +0200

    - fixed: file permissions on /var/www subtree - log directory (/var/log/fabui) is created inside the file bundle rather than during firstboot

--- package/fabui/fabui.mk
+++ package/fabui/fabui.mk
@@ -44,6 +44,8 @@ define FABUI_INSTALL_TARGET_CMDS
 	done
 #	Create runtime data directory
 	$(FABUI_FAKEROOT) $(INSTALL) -d -o 33 -g 33 -m 0755 $(FABUI_TARGET_DIR)/var/lib/fabui
+#	Create log directory
+	$(FABUI_FAKEROOT) $(INSTALL) -d -o 33 -g 33 -m 0755 $(FABUI_TARGET_DIR)/var/log/fabui
 #	The autoinstall flag file is created at compile time
 	$(FABUI_FAKEROOT) touch $(FABUI_TARGET_DIR)/var/www/AUTOINSTALL
 #	We still need a temp directory for fab_ui_security
@@ -51,7 +53,7 @@ define FABUI_INSTALL_TARGET_CMDS
 #	Generate Database
 	$(FABUI_INSTALL_DB)
 #	Fix permissions
-	$(FABUI_FAKEROOT) chown 33:33 -R $(FABUI_TARGET_DIR)/var/www
+	$(FABUI_FAKEROOT) chown -R 33:33 -R $(FABUI_TARGET_DIR)/var/www
 #	Install lighttpd configuration files	
 	$(FABUI_INSTALL_LIGHTTPD_FILES)
 #	Install sudoers permission files
