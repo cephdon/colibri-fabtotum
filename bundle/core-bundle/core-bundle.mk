fd10ebce1939a136cd1e58780d857afb17412bc0
Daniel Kesler <kesler.daniel@gmail.com>
2015-05-05 00:52:55 +0200

    - update: added date and order to version strings - update: added kmod to core-bundle

--- bundle/core-bundle/core-bundle.mk
+++ bundle/core-bundle/core-bundle.mk
@@ -1,9 +1,10 @@
 CORE_BUNDLE_NAME = core
-CORE_BUNDLE_VERSION = 0.1
+CORE_BUNDLE_ORDER = 001
+CORE_BUNDLE_VERSION = v$(shell date +%Y%m%d)
 CORE_BUNDLE_LICENSE = GPLv2
 CORE_BUNDLE_LICENSE_FILES = COPYING
 
-CORE_BUNDLE_PACKAGES = busybox ncurses readline zlib sudo nano dropbear
+CORE_BUNDLE_PACKAGES = busybox kmod ncurses readline zlib sudo nano dropbear
 CORE_BUNDLE_ADD_ROOTFS = YES
 
 $(eval $(bundle-package))
