49ff9472e66a5a89b11979ad6ca51e44d331417c
Daniel Kesler <kesler.daniel@gmail.com>
2015-06-21 01:46:06 +0200

    - added: github link to fabui

--- package/fabui/fabui.mk
+++ package/fabui/fabui.mk
@@ -4,11 +4,9 @@
 #
 ################################################################################
 
-FABUI_VERSION = d29d22ff64cc3822f9b9f6402a4abcbcac50c9be
+FABUI_VERSION = 0562804a6ca72558ab6269c15147ba6365a762ac
 
-#FABUI_SITE = $(call github,FABtotum,Colibri-FAB-UI,$(FABUI_VERSION))
-FABUI_SITE=$(BR2_EXTERNAL)/../Colibri-FAB-UI
-FABUI_SITE_METHOD = local
+FABUI_SITE = $(call github,FABtotum,colibri-fabui,$(FABUI_VERSION))
 
 FABUI_LICENSE = GPLv2
 FABUI_LICENSE_FILES = LICENSE
