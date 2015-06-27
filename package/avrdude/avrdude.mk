9eba24577cf9b9dba6a0aec5bc86fe34222d84c4
Daniel Kesler <kesler.daniel@gmail.com>
2015-06-27 20:59:24 +0200

    - added: avrdude rpi fix

---
+++ package/avrdude/avrdude.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# avrdude-rpi
+#
+################################################################################
+
+# Download avrdude-rpi fixes
+AVRDUDE_EXTRA_DOWNLOADS = \
+	https://raw.github.com/deanmao/avrdude-rpi/master/autoreset \
+	https://raw.github.com/deanmao/avrdude-rpi/master/avrdude-autoreset
+
+AVRDUDE_DEPENDENCIES += python-rpi-gpio strace
+
+define AVRDUDE_FIX_ORIGINAL
+	$(AVRDUDE_FAKEROOT) mv -f $(AVRDUDE_TARGET_DIR)/usr/bin/avrdude $(AVRDUDE_TARGET_DIR)/usr/bin/avrdude-original
+	$(AVRDUDE_FAKEROOT) $(INSTALL) -D -m 755 $(BR2_DL_DIR)/autoreset $(AVRDUDE_TARGET_DIR)/usr/bin/autoreset
+	$(AVRDUDE_FAKEROOT) echo "GPIO.cleanup()" >> $(AVRDUDE_TARGET_DIR)/usr/bin/autoreset
+	$(AVRDUDE_FAKEROOT) $(INSTALL) -D -m 755 $(BR2_DL_DIR)/avrdude-autoreset $(AVRDUDE_TARGET_DIR)/usr/bin/avrdude-autoreset
+	$(AVRDUDE_FAKEROOT) ln -sf /usr/bin/avrdude-autoreset $(AVRDUDE_TARGET_DIR)/usr/bin/avrdude
+endef
+
+AVRDUDE_POST_INSTALL_TARGET_HOOKS += AVRDUDE_FIX_ORIGINAL
