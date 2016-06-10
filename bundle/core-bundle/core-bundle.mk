acfd7903620a5db71d0a1e0a689c6f1c1425a865
Daniel Kesler <kesler.daniel@gmail.com>
2016-06-10 19:28:20 +0200

    - added: squashfs lz4 support for 4.1-qemu kernel - added: missing rsync dependencies (acl, attr) to core-bundle

--- bundle/core-bundle/core-bundle.mk
+++ bundle/core-bundle/core-bundle.mk
@@ -4,7 +4,7 @@ CORE_BUNDLE_VERSION = v$(shell date +%Y%m%d)
 CORE_BUNDLE_LICENSE = GPLv2
 CORE_BUNDLE_LICENSE_FILES = COPYING
 
-CORE_BUNDLE_PACKAGES = busybox dcron dosfstools kmod ncurses readline tzdata fake-hwclock zlib sudo nano dropbear logrotate popt inotify-tools rsync
+CORE_BUNDLE_PACKAGES = busybox dcron dosfstools kmod ncurses readline tzdata fake-hwclock zlib sudo nano dropbear logrotate popt inotify-tools acl attr rsync
 CORE_BUNDLE_ADD_ROOTFS = YES
 
 $(eval $(bundle-package))
