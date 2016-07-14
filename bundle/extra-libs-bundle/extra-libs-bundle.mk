2ee705216e81b5f8588998d9255bbaa0caf33f1f
Daniel Kesler <kesler.daniel@gmail.com>
2016-07-15 01:43:43 +0200

    - added: multimedia-bundle - added: 4.4.y linux config for RPi1 and RPi3 - updated: config.txt to work properly on RPi3

--- bundle/extra-libs-bundle/extra-libs-bundle.mk
+++ bundle/extra-libs-bundle/extra-libs-bundle.mk
@@ -4,7 +4,7 @@ EXTRA_LIBS_BUNDLE_VERSION = v$(shell date +%Y%m%d)
 EXTRA_LIBS_BUNDLE_LICENSE = GPLv2
 EXTRA_LIBS_BUNDLE_LICENSE_FILES = COPYING
 
-EXTRA_LIBS_BUNDLE_PACKAGES = openssl pcre libxml2 libyaml gettext icu libusb libusb-compat elfutils libffi libdaemon expat sqlite
+EXTRA_LIBS_BUNDLE_PACKAGES = openssl pcre libxml2 libyaml gettext icu libusb libusb-compat elfutils libffi libdaemon expat sqlite bzip2
 
 
 $(eval $(bundle-package))
--- bundle/kernel-bundle/kernel-bundle.mk
+++ bundle/kernel-bundle/kernel-bundle.mk
@@ -2,6 +2,6 @@ KERNEL_BUNDLE_NAME = kernel
 KERNEL_BUNDLE_ORDER = 002
 KERNEL_BUNDLE_VERSION = v$(shell date +%Y%m%d)
 
-KERNEL_BUNDLE_PACKAGES = linux linux-firmware
+KERNEL_BUNDLE_PACKAGES = linux linux2 linux-firmware linux-firmware-extra
 
 $(eval $(bundle-package))
